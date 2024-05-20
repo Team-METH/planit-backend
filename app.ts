09d4f8660e9cdcbbdd2c7d82d79e981adcc7b310
DhruvGahoi <dhruvgahoi11111@gmail.com>
2024-05-20 23:34:46 +0530

    feat: add google oauth

--- app.ts
+++ app.ts
@@ -8,9 +8,18 @@ const serverStartDate = Date.now();
 const PORT = 8000;
 const connectDB = require("./db/conn");
 const logger = require("./utils/logger");
+const auth = require("./routes/auth")
 connectDB();
 
 app.use(boom());
+
+app.get("/privacy", (req: Request, res: Response) => {
+  return res.json({message : "privacy page"})
+})
+
+app.get("/tos", (req: Request, res: Response) => {
+  return res.json({message : "tos page"})
+})
 
 app.get("/health", (req: Request, res: Response) => {
   const healthTimeInSeconds = (Date.now() - serverStartDate) / 1000;
@@ -23,10 +32,13 @@ app.get("/health", (req: Request, res: Response) => {
   });
 });
 
+app.use(auth);
+
 // for not found pages - 404 error page
 app.use(function (req: Request, res: CustomResponse) {
   res.boom.notFound();
 });
+
 
 app.listen(PORT, (error: Error) => {
   if (error) console.error();
